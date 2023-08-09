8e3f63cb5cc50af48b32899d71c8dc732b34f3ce
Luke <lukeweerheim@hotmail.com>
2023-08-09 14:26:42 +0200

    fix: Rework QueryInclusion type to allow array types

--- src/classes/Search.ts
+++ src/classes/Search.ts
@@ -1,5 +1,5 @@
 import { Client } from '..';
-import { AllowedQuery, QueryResult, QueryResults } from '../constants/queries';
+import { AllowedQuery, QueryInclusion, QueryResult, QueryResults } from '../constants/queries';
 import { AniListReturnable } from '../types/aniList';
 import { Query } from '../util/Query';
 
@@ -12,8 +12,8 @@ export class Search<R extends AniListReturnable> {
     this.type = type;
   }
 
-  async getById<K extends Extract<keyof R, string>>(id: number, include: K[]): Promise<QueryResult<K, R>> {
-    const query = new Query(this.type);
+  async getById<K extends Extract<keyof R, string>>(id: number, include: QueryInclusion<R>): Promise<QueryResult<K, R>> {
+    const query = new Query<R>(this.type);
     query.setInclude(include);
     query.setVariables({
       id,
@@ -21,16 +21,16 @@ export class Search<R extends AniListReturnable> {
       perPage: 25,
     });
 
-    const result = await this.client.fetcher.fetch(query, false);
+    const result = await this.client.fetcher.fetch<R, false>(query, false);
     return result[0] as unknown as QueryResult<K, R>;
   }
 
   async getByQuery<T extends boolean, K extends Extract<keyof R, string>>(
     name: string,
     showPageInfo: T,
-    include: Extract<keyof R, string>[],
+    include: QueryInclusion<R>,
   ): Promise<QueryResults<T, K, R>> {
-    const searchQuery = new Query(this.type);
+    const searchQuery = new Query<R>(this.type);
     searchQuery.setInclude(include);
     searchQuery.setVariables({
       name,
@@ -38,7 +38,7 @@ export class Search<R extends AniListReturnable> {
       perPage: 25,
     });
 
-    const result = await this.client.fetcher.fetch(searchQuery, showPageInfo);
+    const result = await this.client.fetcher.fetch<R, T>(searchQuery, showPageInfo);
     return result as unknown as QueryResults<T, K, R>;
   }
 }
